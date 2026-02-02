78d3c2a1c9bab590640a53d9bc3e21e68957b580
josueportiz <josueportiz@gmail.com>
2026-02-02 16:00:44 +0100

    Update doc

--- api/search/defines_c.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tick_5ffreq_5f1khz_715',['TICK_FREQ_1KHZ',['../stm32f4__system_8c.html#aafb4caba8017f1b553bd78f60ecd11c4',1,'stm32f4_system.c']]]
-];
--- api/search/typedefs_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['fsm_5fbutton_5ft_295',['fsm_button_t',['../fsm__button_8h.html#aec6bc62ec1cc0ab65e153d018cafd633',1,'fsm_button.h']]],
-  ['fsm_5fkeyboard_5ft_296',['fsm_keyboard_t',['../fsm__keyboard_8h.html#aeb9d199f25d41dc00d3aafb7bf34d934',1,'fsm_keyboard.h']]]
-];
